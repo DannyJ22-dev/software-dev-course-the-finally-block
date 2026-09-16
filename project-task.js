8a0bd1d96be8c1931c79e1eaae32fec74d12344d
Danny <dannymcjensen@gmail.com>
2026-09-16 17:39:25 -0600

    Practice finished

--- project-task.js
+++ project-task.js
@@ -45,7 +45,15 @@ Step 4: Test Your Solution
 function processFile(fileName, fileData) {
   try {
     // TODO: Add input validation here
-    
+    if (!fileName){
+        throw new Error("File name is missing");
+    } 
+    if (typeof fileData !== "string"){
+        throw new Error("File data must be a string");
+    }
+    if (!fileData){
+      throw new Error("File data is missing");
+    }
     // TODO: Implement simulated file processing here
     console.log(`Processing file: ${fileName}`);
     console.log(`File content: ${fileData}`);
@@ -54,7 +62,9 @@ function processFile(fileName, fileData) {
     
   } catch (err) {
     // TODO: Implement error handling
-    console.error(err);
+    console.error(err.message);
+  } finally {
+    console.log(`file released: ${fileName}`);
   }
   // TODO: Implement a finally block to close resources
 }
@@ -63,7 +73,7 @@ function processFile(fileName, fileData) {
 // 🧪 Test Cases Below
 // ============================================
 
-processFile(); // ❌ ReferenceError: File name is missing
-processFile("myFile.txt", 42); // ❌ TypeError: File data must be a string
-processFile("myFile.txt", ""); // ❌ Error: File data cannot be empty
-processFile("myFile.txt", "Hello, world!"); // ✅ Should process successfully
+processFile("", "Goodbye, world"); // ❌ ReferenceError: File name is missing
+processFile("file2.txt", 42); // ❌ TypeError: File data must be a string
+processFile("file3.txt", ""); // ❌ Error: File data cannot be empty
+processFile("file4.txt", "Hello, world!"); // ✅ Should process successfully
